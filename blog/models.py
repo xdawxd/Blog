1d6f970b3767c451775f31c0ac2e1fccacd1ddbf
Dawid Schwinge <259639@student.pwr.edu.pl>
2021-05-04 22:38:46 +0200

    Created models and added the time zone
    Created two main models: Post and Comment
    Added European time zone

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    creation_date = models.DateTimeField(default=timezone.now())
+    publication_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    # once post is created the user will be redirected to 'post_detail' view
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    creation_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    # once post is created the user will be redirected to 'posts' which will be a name of 'post_list' view
+    def get_absolute_url(self):
+        return reverse('posts', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.text
