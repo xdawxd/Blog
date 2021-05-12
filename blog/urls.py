74f41523f5173617be5ae63f82107a1e09431c83
Dawid Schwinge <259639@student.pwr.edu.pl>
2021-05-12 21:48:38 +0200

    Rewrote comment_views to Class-based views

--- blog/urls.py
+++ blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('post/<int:pk>/remove/', post_views.PostDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/publish/', post_views.post_publish, name='post_publish'),
     path('drafts/', post_views.DraftListView.as_view(), name='draft_list'),
-    path('post/<int:pk>/comment/', comment_views.comment_add, name='comment_add'),
-    path('comment/<int:pk>/approve/', comment_views.comment_approve, name='comment_approve'),
-    path('comment/<int:pk>/remove', comment_views.comment_remove, name='comment_remove'),
+    path('post/<int:pk>/comment/', comment_views.CommentView.as_view(), name='comment_add'),
+    path('comment/<int:pk>/approve/', comment_views.CommentApprove.as_view(), name='comment_approve'),
+    path('comment/<int:pk>/remove', comment_views.CommentRemove.as_view(), name='comment_remove'),
 ]
--- blog/views/comment_views.py
+++ blog/views/comment_views.py
@@ -1,39 +1,42 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from blog.models import Post, Comment
 from blog.forms import CommentForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 
-# rewrite to classes later on...
-@login_required
-def comment_add(request, pk):
-    post = get_object_or_404(Post, pk=pk)
 
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
+class CommentView(LoginRequiredMixin, View):
+    form_class = CommentForm
+    model = Comment
+    template_name = 'blog/comment_form.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+
+        return render(request, self.template_name, context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        post = get_object_or_404(Post, pk=kwargs['pk'])
 
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
 
-            return redirect('blog:post_detail', pk=post.pk)
-
-    else:
-        form = CommentForm()
-
-    return render(request, 'blog/comment_form.html', context={'form': form})
+            return redirect('blog:post_detail', pk=kwargs['pk'])
 
 
-@login_required
-def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
-    return redirect('blog:post_detail', pk=comment.post.pk)
+class CommentApprove(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        comment.approve()
+        return redirect('blog:post_detail', pk=comment.post.pk)
 
 
-@login_required
-def comment_remove(request, pk):
-    comment =  get_object_or_404(Comment, pk=pk)
-    post_pk = comment.post.pk
-    comment.delete()
-    return redirect('blog:post_detail', pk=post_pk)
+class CommentRemove(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        comment =  get_object_or_404(Comment, pk=kwargs['pk'])
+        post_pk = comment.post.pk
+        comment.delete()
+        return redirect('blog:post_detail', pk=post_pk)
